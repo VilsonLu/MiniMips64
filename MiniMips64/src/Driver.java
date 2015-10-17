7068fe0b10ea8846cafef38239c7929a5babace0
vilsonluffy <vilson_lu@yahoo.com>
2015-10-18 02:33:53 +0800

    Testing

--- MiniMips64/src/Driver.java
+++ MiniMips64/src/Driver.java
@@ -5,6 +5,7 @@ public class Driver {
 		// TODO Auto-generated method stub
 		try {
 			System.out.println(Helper.BinaryToHex("11001111"));
+			System.out.println(Helper.BinaryToHex("11000110"));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
