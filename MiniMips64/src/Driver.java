40ef36e3c553a70967653aac6c0d21382bcb3496
vilsonluffy <vilson_lu@yahoo.com>
2015-10-18 02:31:13 +0800

    Baseline
    IntToBinary
    BinaryToHex

---
+++ MiniMips64/src/Driver.java
@@ -0,0 +1,14 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println(Helper.BinaryToHex("11001111"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ MiniMips64/src/Helper.java
@@ -0,0 +1,38 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+
+public class Helper {
+	private static int HEXNUM =4;
+	public static String IntToBinary(int number){
+		return Integer.toBinaryString(number);
+	}
+	
+	public static String BinaryToHex(String binary) throws Exception{
+		int binLength = binary.length();
+		if(binLength % 4 == 0){
+			String hex = "";
+			int counter = 1;
+			String temp = "";
+			
+			for(int i = 0; i<binLength; i++){
+				if(counter < HEXNUM){
+					temp += binary.charAt(i);
+					
+				} else {
+					temp += binary.charAt(i);
+					hex += (new BigInteger(temp,2)).toString(16);
+					counter = 0;
+					temp = "";
+				}
+				counter++;
+			}
+			
+			return hex;
+		} else {
+			// we can throw exception here
+			throw new Exception("Cannot convert to hex");
+		}
+	}
+	
+}
