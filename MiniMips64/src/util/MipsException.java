3acf132dd7accf28447016e518555902431a8fd6
Ivan Paner <Ivan Paner@10.40.5.40>
2015-10-20 13:36:37 +0800

    Added Base Exception.

---
+++ MiniMips64/src/util/MipsException.java
@@ -0,0 +1,13 @@
+package util;
+
+public class MipsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1605676999692622356L;
+	
+	public MipsException(String message) {
+		super(message);
+	}
+}
