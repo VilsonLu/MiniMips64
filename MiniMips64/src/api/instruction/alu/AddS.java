9287f9ce57c82945b95cb6eb45fc2a7baacb0f2e
Ivan Paner <ivan.paner@gmail.com>
2015-11-29 23:01:03 +0800

    Implemented floats in Pipeline.

---
+++ MiniMips64/src/api/instruction/alu/AddS.java
@@ -0,0 +1,17 @@
+package api.instruction.alu;
+
+public class AddS extends AluInstruction {
+
+	@Override
+	long getExOperation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getStringCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- MiniMips64/src/api/pipeline/Pipeline.java
+++ MiniMips64/src/api/pipeline/Pipeline.java
@@ -6,21 +6,33 @@ import java.util.Map;
 
 import api.instruction.Instruction;
 import api.instruction.InstructionMgr;
+import api.instruction.alu.AddS;
+import api.instruction.alu.MulS;
 import api.register.RegisterMgr;
 
 public class Pipeline {
 	private final int FP_ADD_CYCLES = 4;
 	private final int FP_MULT_CYCLES = 8;
 	
-	private Instruction if_; // ife(tch) because if is a reserved keyword and I don't want to use capital letters for variables.
+	private Instruction if_; // because if is a reserved keyword and I don't want to use capital letters for variables.
 	private Instruction id;
 	private Instruction ex;
 	private Instruction mem;
 	private Instruction wb;
-	private List<Instruction> fpadd;
-	private List<Instruction> fpmult;
+	private Instruction[] fpadds;
+	private Instruction[] fpmults;
 	private long cycle = 0;
 	private boolean stall;
+	private Exec lastExec;
+	
+	private enum Exec {
+		EX, FP_ADD, FP_MULT
+	}
+	
+	public Pipeline() {
+		fpadds = new Instruction[FP_ADD_CYCLES];
+		fpmults = new Instruction[FP_MULT_CYCLES];
+	}
 	
 	
 	public void printContents() {
@@ -45,14 +57,12 @@ public class Pipeline {
 			System.out.println();
 		}
 		
-		
 		System.out.print("MEM: ");
 		if (mem != null) {
 			System.out.println(mem.getStringCode());
 		} else {
 			System.out.println();
 		}
-		
 		
 		System.out.print("WB: ");
 		if (wb != null) {
@@ -73,24 +83,72 @@ public class Pipeline {
 	
 	private void moveRegistersForward() {
 		wb = mem;
-		mem = ex;
-		if (!stall) {
-			ex = id;
-			id = if_;
-			if_ = this.getNextInstruction();	
-		} else {
+		
+		// move last executed instruction to MEM 
+		switch (lastExec) {
+		case EX:
+			mem = ex;
 			ex = null;
+			break;
+		case FP_ADD:
+			mem = fpadds[FP_ADD_CYCLES - 1];
+			fpadds[FP_ADD_CYCLES - 1] = null;
+			break;
+		case FP_MULT:
+			mem = fpmults[FP_MULT_CYCLES - 1];
+			fpmults[FP_MULT_CYCLES - 1] = null;
+			break;
 		}
 		
+		// move FP ADDs
+		for (int i = FP_ADD_CYCLES; i == 0; i--) {
+			if (fpadds[i] == null) {
+				fpadds[i] = fpadds[i - 1];
+				fpadds[i - 1] = null;
+			}
+		}
+		
+		// move FP MULs
+		for (int i = FP_MULT_CYCLES; i == 0; i--) {
+			if (fpmults[i] == null) {
+				fpmults[i] = fpmults[i - 1];
+				fpmults[i - 1] = null;
+			}
+		}
+		
+		if (!stall) {
+			// move ID to appropriate Execution stage
+			if (id instanceof AddS) {
+				if (fpadds[0] == null) { // don't combine with the above expression!  
+					fpadds[0] = id;	
+				}
+			} else if (id instanceof MulS) {
+				if (fpmults[0] == null) {
+					fpmults[0] = id;	
+				}
+			} else if (ex == null) {
+				ex = id;
+			} else {
+				stall = true; // stall because appropriate Execution stage is full 
+			}
+			
+			if (!stall) {
+				id = if_;
+				if_ = this.getNextInstruction();	
+			}
+		}
 	}
 	
 	
 	private void checkForStall() {
 		List<String> inputs = null;
+		stall = false;
 		if (id != null) {
 			inputs = id.getInputs();
+		} else {
+			return;
 		}
-		stall = false; 
+		 
 		if (ex != null) {
 			stall = ex.outputsTo(inputs);
 		}
@@ -99,6 +157,25 @@ public class Pipeline {
 		}
 		if (!stall && wb != null) {
 			stall = wb.outputsTo(inputs);
+		}
+		if (!stall) {
+			for (int i = 0; i < FP_ADD_CYCLES; i++) {
+				Instruction fpadd = fpadds[i];
+				if (fpadd == null)  
+					continue;
+				stall = fpadd.outputsTo(inputs);
+				if (stall) break;
+			}
+		}
+		if (!stall) {
+			for (int i = 0; i < FP_MULT_CYCLES; i++) {
+				Instruction fpmult = fpmults[i];
+				if (fpmult == null) 
+					continue;
+				stall = fpmult.outputsTo(inputs);
+				if (stall) 
+					break;
+			}
 		}
 	}
 	
@@ -109,9 +186,33 @@ public class Pipeline {
 		if (mem != null) {
 			mem.mem();	
 		}
+		
+		// check which Exec will execute based on Instruction id
+		Exec toExecute = Exec.EX;
+		int minId = Integer.MAX_VALUE;
 		if (ex != null) {
-			ex.ex();	
+			minId = ex.getId();
 		}
+		Instruction fpaddLast = fpadds[FP_ADD_CYCLES - 1];
+		Instruction fpmultLast = fpmults[FP_MULT_CYCLES - 1];
+		
+		if (fpaddLast != null && fpaddLast.getId() < minId) {
+			minId = fpaddLast.getId();
+			toExecute = Exec.FP_ADD;
+		}
+		if (fpmultLast != null && fpmultLast.getId() < minId) {
+			minId = fpmultLast.getId();
+			toExecute = Exec.FP_MULT;
+		}
+				
+		if (toExecute.equals(Exec.EX) && ex != null) {
+			ex.ex();	
+		} else if (toExecute.equals(Exec.FP_ADD)) {
+			fpaddLast.ex();
+		} else if (toExecute.equals(Exec.FP_MULT)){
+			fpmultLast.ex();
+		}
+		
 		if (id != null && !stall) {
 			id.id();
 		}
@@ -119,6 +220,7 @@ public class Pipeline {
 			if_.ife();
 		}
 	
+		lastExec = toExecute;
 	}
 	
 	public long getCycle() {
@@ -144,6 +246,17 @@ public class Pipeline {
 		if (wb != null) {
 			result.put(wb, "WB");
 		}
+		for (int i = 0; i < FP_ADD_CYCLES; i++) {
+			if (fpadds[i] != null) {
+				result.put(fpadds[i], "A" + i);
+			}
+		}
+		for (int i = 0; i < FP_MULT_CYCLES; i++) {
+			if (fpmults[i] != null) {
+				result.put(fpmults[i], "M" + i);
+			}
+		}
+		
 		return result;
 	}
 	
