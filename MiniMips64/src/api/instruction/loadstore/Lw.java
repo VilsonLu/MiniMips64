bbf281c799c50b41611da2e4ae37112b8ff365bf
Ivan Paner <ivan.paner@gmail.com>
2015-12-01 07:46:46 +0800

    LW implemented. Fixed S.S opcode string name.

--- MiniMips64/src/api/instruction/loadstore/Lw.java
+++ MiniMips64/src/api/instruction/loadstore/Lw.java
@@ -4,52 +4,48 @@ import api.instruction.opcode.Itype;
 import api.memory.MemoryMgr;
 import api.register.RegisterMgr;
 
-public class Ss extends LoadStoreInstruction {
+public class Lw extends LoadStoreInstruction {
 	private Itype opcode;
 	
-	public Ss(String registerString) {
+	public Lw(String registerString) {
 		super();
-		opcode = new Itype("S.S", 57);
+		opcode = new Itype("LW", 35);
 		this.setOpcode(opcode);
 		String registers[] = registerString.split(","); // rd, offset, rs
 		opcode.setRt(registers[0]);
 		opcode.setImm(registers[1]);
 		opcode.setRs(registers[2]);
+		opcode.setDestination("r" + registers[0]);
 		System.out.println(opcode.getBinaryCode());
 	}
 	
 	
 	@Override
-	public void id() {
-		super.id();
-		RegisterMgr regs = RegisterMgr.getInstance();
-		long a = regs.getFValue(opcode.getRs());
-		long b = regs.getFValue(opcode.getRt());
-		regs.setValue(RegisterMgr.ID_EX_A, a);
-		regs.setValue(RegisterMgr.ID_EX_B, b);
-	}
-	
-
-	@Override
 	public String getStringCode() {
 		return opcode.getStringCodeRegOffsetReg();
 	}
 
+	
 	@Override
 	public void mem() {
 		MemoryMgr mems = MemoryMgr.getInstance();
 		RegisterMgr regs = RegisterMgr.getInstance();
 		long location = regs.getOldValue(RegisterMgr.EX_MEM_ALUOUTPUT);
-		long b = regs.getValue(RegisterMgr.EX_MEM_B);
+		long lmd = 0;
 		for (int i = 0; i < 4; i++) {
-			byte value = (byte) ((byte) b % 0x100);
-			b = b / 0x100;
-			mems.set(location + 1, value);
+			byte value = mems.get(location + i);
+			long converted = value << (i * 8);
+			lmd = lmd + converted;
 		}
+		regs.setValue(RegisterMgr.MEM_WB_LMD, lmd);
+
 	}
 
+	
 	@Override
 	public void wb() {
+		RegisterMgr regs = RegisterMgr.getInstance();
+		long value = regs.getOldValue(RegisterMgr.MEM_WB_LMD);
+		regs.setValue(opcode.getDestination(), value);
 	}
-
 }
